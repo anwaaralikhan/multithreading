11eb902505fd4af54bc748763d93094804530d2d
Digital Alchemist <38991296+anwaaralikhan@users.noreply.github.com>
2019-11-09 14:52:59 -0500

    Create bug_join_inside_method.java

---
+++ usecases/join/bug_join_inside_method.java
@@ -0,0 +1,36 @@
+public class TestJoin {
+	public static void main(String[] args) {
+        JoinItself j = new JoinItself();
+        System.out.println(j.isAlive());
+        j.start();
+        System.out.println(j.isAlive());
+        System.out.println("Thread started ...");
+    }
+}
+class JoinItself extends Thread {
+    public void run() {
+        System.out.println("Inside the run method ");
+        System.out.println(Thread.currentThread().isAlive());
+        for(int i=0;i<5;i++) {
+            try {
+            	System.out.println(" Value :" +i);
+                System.out.println("Joining itself ...");
+                Thread.currentThread().join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+/***
+
+false
+true
+Thread started ...
+Inside the run method 
+true
+ Value :0
+Joining itself ...
+
+***/
